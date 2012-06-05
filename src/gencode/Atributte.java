4f17f6640c2485e259ba7ac273803cc4be2b5bd7
Abilio Gambim Parada <abiliogp@gmail.com>
2012-06-05 18:23:47 -0300

    default value e imports ok

--- src/gencode/Atributte.java
+++ src/gencode/Atributte.java
@@ -103,32 +103,30 @@ public class Atributte extends DataModel {
 	}
 	
 	public void genCodeImports(BufferedWriter out) throws IOException{
-			
-			if (Android.os.contains(this.name)) {
-				out.write("import android.os." + name + ";\n");
-			}
-			if (Android.Widget.contains(name)) {
-				out.write("import android.widget." + name + ";\n");
-			}
-			if (Android.View.contains(name)) {
-				out.write("import android.view." + name + ";\n");
-			}
-			if (Android.view.contains(name)) {
-				out.write("import android.view.View." + name + ";\n");
-			}
-			if (Android.Content.contains(name)) {
-				out.write("import android.content." + name + ";\n");
-			}
-			if (Android.res.contains(name)) {
-				out.write("import android.content.res." + name + ";\n");
-			}
-			
-			if (Android.adntroidUtil.contains(name)) {
-				out.write("import android.util." + name + ";\n");
-			}
-			if (Android.javaUtil.contains(name)) {
-				out.write("import java.util." + name + ";\n");
-			}
+		if (Android.os.contains(this.name)) {
+			out.write("import android.os." + name + ";\n");
+		}
+		else if (Android.Widget.contains(name)) {
+			out.write("import android.widget." + name + ";\n");
+		}
+		else if  (Android.View.contains(name)) {
+			out.write("import android.view." + name + ";\n");
+		}
+		else if (Android.view.contains(name)) {
+			out.write("import android.view.View." + name + ";\n");
+		}
+		else if (Android.Content.contains(name)) {
+			out.write("import android.content." + name + ";\n");
+		}
+		else if (Android.res.contains(name)) {
+			out.write("import android.content.res." + name + ";\n");
+		}
+		else if (Android.adntroidUtil.contains(name)) {
+			out.write("import android.util." + name + ";\n");
+		}
+		else if (Android.javaUtil.contains(name)) {
+			out.write("import java.util." + name + ";\n");
+		}
 	}
 
 	/**
